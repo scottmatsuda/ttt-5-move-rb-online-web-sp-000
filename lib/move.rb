2954516bae4ee09d25b34b7f04ed3205a39b4ea6
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-12 00:11:26 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,9 +9,8 @@ end
 # code your input_to_index and move method here!
 def input_to_index(input)
   input_integer = input.to_i
-  display_board(board)
-  position = board[input_integer - 1]
-  return position
+  display_board(board[input_integer - 1])
+  
 end
 
 def move  
